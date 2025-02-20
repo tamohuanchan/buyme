216cba093052c8e23bc231f60b481f4faf44be04
tamohuanchan <tamohuanchan@yandex.ru>
2025-02-20 19:42:25 +0300

    изменения бд

--- app.py
+++ app.py
@@ -41,6 +41,47 @@ def about():
 # def user(user_name):
 #     return render_template("user.html")
 
+@app.route("/feed")
+def feed():
+    positions = Positions.query.order_by(Positions.published_at.desc()).all()
+
+    return render_template("feed.html", positions=positions)
+
+@app.route("/feed/<string:id>")
+def position(id):
+    position = Positions.query.get(id)
+    return render_template("position.html", position=position)
+
+@app.route("/feed/<string:id>/delete")
+def position_delete(id):
+    position = Positions.query.get_or_404(id)
+    try:
+        db.session.delete(position)
+        db.session.commit()
+        return redirect('/feed')
+    except Exception as e:
+        return f"Произошла ошибка: {e}"
+
+
+@app.route("/feed/<string:id>/update", methods=["POST", "GET"])
+def position_update(id):
+    position = Positions.query.get_or_404(id)  # Получаем объект из БД или 404
+
+    if request.method == "POST":
+        position.title = request.form['title']
+        position.description = request.form['description']
+        images_data = request.form.get('images', '').strip()
+        position.images = images_data if images_data else None  # Обновляем изображения
+
+        try:
+            db.session.commit()  # Фиксируем изменения
+            return redirect('/feed')
+        except Exception as e:
+            return f"Произошла ошибка: {e}"
+
+    return render_template("position_update.html", position=position)
+
+
 @app.route("/create_position", methods=["POST", "GET"])
 def create_position():
     if request.method == "POST":
@@ -56,7 +97,7 @@ def create_position():
         try:
             db.session.add(position)
             db.session.commit()
-            return redirect('/')
+            return redirect('/feed')
         except Exception as e:
             return f"Произошла ошибка: {e}"
 
@@ -64,3 +105,5 @@ def create_position():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
